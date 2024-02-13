7356b57d8bf42d985c2ac5da45c5a94be057e765
NoelleDeAngelo <NoelleDeAngelo@gmail.com>
2024-02-13 14:04:44 -0800

    Change width of screen to width of device

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,6 +12,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta name ='viewport' contents='width=device-width, inital-scale=1.0'/>
+      </head>
       <body>
         <Nav/>
         {children}
