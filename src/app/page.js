f75062398c10df547e18a7225c44d1091d1a9775
NoelleDeAngelo <NoelleDeAngelo@gmail.com>
2024-06-15 11:35:15 -0700

    Add background image to hero section

--- src/app/page.js
+++ src/app/page.js
@@ -28,25 +28,27 @@ const styles = stylex.create({
     minWidth: '200px',
   },
   heroImage:{
-    //background:'black',
-    backgroundImage: "url('https://images.unsplash.com/photo-1512438248247-f0f2a5a8b7f0?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
-    //width: '100%',
-    //height:'100%',
+    backgroundImage: "url('/images/sunsetman.jpg')",
     backgroundPosition: 'center',
     backgroundRepeat: 'no-repeat',
     backgroundSize: 'cover',
     position: 'relative',
   },
   header:{
-    fontWeight:'300',
+    fontWeight:'800',
+    letterSpacing: '.6px',
     textAlign: 'center',
+    fontSize: {
+      default:'2.5rem',
+      '@media (width <= 750px)':'1.8rem',
+    },
   },
 
   link:{
     padding:'5vh 0vh',
     color: colors.headline,
     fontSize: {
-      default:'1.3rem',
+      default:'1.5rem',
       '@media (width <= 750px)':'1.2rem',
     },
     fontStyle: 'italic',
@@ -68,7 +70,7 @@ export default function Home() {
         <div {...stylex.props(styles.hero)}>
 
           <h1 {...stylex.props(styles.header)}>Embark on a Transformative Journey with Our Intuitive Life Coaching</h1>
-          <Link href= 'https://wisdomweavercoaching.youcanbook.me/' {...stylex.props(styles.link)}>Start Your Journey Today</Link>
+          <Link href= 'https://wisdomweavercoaching.youcanbook.me/' {...stylex.props(styles.link)}>Book a Free Consultaion</Link>
         </div>
       </div>
       <div {...stylex.props(styles.section)}>
