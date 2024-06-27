dd334fa655ebe6926aa62484ef43ca5f47de4839
NoelleDeAngelo <NoelleDeAngelo@gmail.com>
2024-06-27 10:52:53 -0700

    Move about section to its own component

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,8 @@ import * as stylex from "@stylexjs/stylex";
 import Nav from "../components/Nav.js";
 import { colors, spacing, fonts } from "../tokens.stylex";
 import Link from "next/link";
-import Image from "next/image";
+import About from "../components/About.js";
+
 
 
 const styles = stylex.create({
@@ -55,75 +56,6 @@ const styles = stylex.create({
     },
     fontWeight: "300",
   },
-  aboutSection: {
-    display: "flex",
-    flexFlow: "row wrap",
-    minHeight: "450px",
-  },
-  aboutImageContainer: {
-    display: "flex",
-    flex: "1 0 100px",
-    alignItems: "center",
-    justifyContent: "center",
-    background: colors.lightBackground,
-    minHeight: "250px",
-  },
-  aboutImage: {
-    borderRadius: "50%",
-    position: "relative",
-    left: {
-      default: "50%",
-      "@media (width <= 799px)": "0px",
-    },
-    outline: "6px solid",
-    outlineColor: colors.lightAccent,
-    outlineOffset: "4px",
-    width: {
-      default: "275px",
-      "@media (850px < width <= 950px)": "250px",
-      "@media (width <= 850px)": "200px",
-    },
-    height: {
-      default: "275px",
-      "@media (850px < width <= 950px)": "250px",
-      "@media (width <= 850px)": "200px",
-    },
-  },
-  about: {
-    display: "flex",
-    flexDirection: "column",
-    background: "white",
-    flex: "3 1 600px",
-    justifyContent: "center",
-    alignItems: {
-      default: null,
-      "@media (width <= 950px)": "center",
-    },
-  },
-  aboutHeading: {
-    color: colors.lightHeading,
-    marginLeft: {
-      default: "21%",
-      "@media (800px < width <= 9500px)": "15%",
-      "@media (width <= 800px)": "0",
-    },
-    marginBottom: "0",
-  },
-  aboutText: {
-    color: colors.lightText,
-    marginTop: "10px",
-    marginBottom: "10px",
-    marginRight: "10%",
-    marginLeft: {
-      default: "25%",
-      "@media (width <= 800px)": "10%",
-    },
-    maxWidth: "700px",
-    textAlign: {
-      default: "left",
-      "@media (width <= 950px)": "center",
-    },
-  },
 });
 
 export default function Home() {
@@ -142,36 +74,7 @@ export default function Home() {
           </Link>
         </div>
       </div>
-      <div {...stylex.props(styles.aboutSection)}>
-        <div {...stylex.props(styles.aboutImageContainer)}>
-          <Image
-            {...stylex.props(styles.aboutImage)}
-            src="/images/Araceli.png"
-            width={275}
-            height={275}
-            alt="Wisdom Weaver Coaching Logo"
-          />
-        </div>
-        <div {...stylex.props(styles.about)}>
-          <h2 {...stylex.props(styles.aboutHeading)}>Who We Are</h2>
-          <p {...stylex.props(styles.aboutText)}>
-            At Wisdom Weaver Coaching, we are dedicated to helping individuals
-            unleash their inner greatness and live authentically. Founded by
-            Araceli Lopez, our coaching practice is built upon a foundation of
-            passion, expertise, and a genuine commitment to supporting others in
-            their personal and professional growth. As a certified coach, I have
-            helped countless clients overcome challenges, break through
-            limitations, and create meaningful change in their lives. Our
-            mission is simple: to inspire and empower you to become the best
-            version of yourself. Whether you're navigating a career transition,
-            seeking to improve your well-being, or striving for greater
-            fulfillment, we are here to provide the guidance, support, and
-            encouragement you need to thrive. Get to know us more and discover
-            how our transformative coaching services can help you unlock your
-            potential and create a life you love
-          </p>
-        </div>
-      </div>
+      <About/>
     </div>
   );
 }
