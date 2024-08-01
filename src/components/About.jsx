3ce69b3276f48685d6cff769603c98b37b7b35f4
NoelleDeAngelo <NoelleDeAngelo@gmail.com>
2024-08-01 14:28:34 -0700

    Change About text

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -92,22 +92,32 @@ export default function About() {
         />
       </div>
       <div {...stylex.props(styles.about)}>
-        <h2 {...stylex.props(styles.aboutHeading)}>Who We Are</h2>
+        <h2 {...stylex.props(styles.aboutHeading)}>Who I Am</h2>
         <p {...stylex.props(styles.aboutText)}>
-          At Wisdom Weaver Coaching, we are dedicated to helping individuals
-          unleash their inner greatness and live authentically. Founded by
-          Araceli Lopez, our coaching practice is built upon a foundation of
-          passion, expertise, and a genuine commitment to supporting others in
-          their personal and professional growth. As a certified coach, I have
-          helped countless clients overcome challenges, break through
-          limitations, and create meaningful change in their lives. Our mission
-          is simple: to inspire and empower you to become the best version of
-          yourself. Whether you&apos;re navigating a career transition, seeking
-          to improve your well-being, or striving for greater fulfillment, we
-          are here to provide the guidance, support, and encouragement you need
-          to thrive. Get to know us more and discover how our transformative
-          coaching services can help you unlock your potential and create a life
-          you love.
+          With over 30 years of experience in the human development and public
+          service sector, I have dedicated my career to supporting individuals
+          from diverse backgrounds. My education and career have spanned various
+          roles, working with a wide range of populations, which has enriched my
+          understanding of human potential and resilience. Additionally, as a
+          child of immigrant parents of native Mexican descent, I developed a
+          deep appreciation for the wisdom held sacred among indigenous cultures
+          around the world.
+        </p>
+        <p {...stylex.props(styles.aboutText)}>
+          In 2017, my life took a surprising turn when my husband and I
+          relocated to the Big Island of Hawaii, seeking healing and a better
+          life balance. During a life-threatening health crisis that left my
+          husband in a coma, I experienced a pivotal moment of insight that
+          helped bring him back, defying medical expectations. This profound
+          experience ignited a new purpose within me: to share my insights and
+          support others in their journeys.
+        </p>
+        <p {...stylex.props(styles.aboutText)}>
+          Certified as an innate well-being specialist through the International
+          Guild of Hypnotherapy and trained in NLP and the Three Principles (3P)
+          by David Key, my coaching style combines mindset shifts and practical
+          tools to empower individuals to navigate life&apos;s challenges with
+          resilience and clarity.
         </p>
       </div>
     </div>
